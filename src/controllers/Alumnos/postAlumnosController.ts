c6f08c7a54412dbf29dc3b18f63493f20057fb4a
Ignacio Martin <119757609+maximustxt@users.noreply.github.com>
2023-11-15 21:29:33 -0300

    cambios

--- src/controllers/Alumnos/postAlumnosController.ts
+++ src/controllers/Alumnos/postAlumnosController.ts
@@ -4,19 +4,13 @@ import Alumno from "../../models/Alumno";
 //*- Interfaces :
 import { IAlumno } from "../../../common/Interfaces";
 
-const postAlumnosController = async (P_Alumno: IAlumno, idCurso: string) => {
+const postAlumnosController = async (P_Alumno: IAlumno) => {
   try {
     const { nombre } = P_Alumno;
 
     const alumno = await Alumno.findOne({ nombre }).exec();
 
     if (alumno) {
-      // Agregar a los cursos del Alumno el nuevo curso :
-      const objectIdCurso = new mongoose.Types.ObjectId(idCurso);
-      alumno.cursos.push(objectIdCurso);
-
-      // Guardamos los cambios :
-      await alumno.save();
       throw new Error("Alumno ya creado!");
     } else {
       const newAlumno = new Alumno(P_Alumno);
