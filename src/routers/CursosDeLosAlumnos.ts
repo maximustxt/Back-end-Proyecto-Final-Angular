f21e56ba73b5816015c291ccc2b8450d35f6e39b
Ignacio Martin <119757609+maximustxt@users.noreply.github.com>
2023-11-18 05:42:48 -0300

    cambios

--- src/routers/CursosDeLosAlumnos.ts
+++ src/routers/CursosDeLosAlumnos.ts
@@ -4,6 +4,8 @@ const CursosDeLosAlumnos = Router();
 //* CONTROLLERS :
 import getCursosDelAlumnoController from "../controllers/Cursos_De_Los_Alumnos/getCursosDelAlumnoController";
 import postCursosDelAlumnoController from "../controllers/Cursos_De_Los_Alumnos/postCursosDelAlumnoController";
+import deleteCursosDelAlumnoController from "../controllers/Cursos_De_Los_Alumnos/deleteCursosDelAlumnoController";
+import putCursosDelAlumnoController from "../controllers/Cursos_De_Los_Alumnos/putCursosDelAlumnoController";
 
 /!*------------------------------ OBTENER CURSOS DE LOS ALUMNOS ---------------------------------*/;
 
@@ -25,6 +27,33 @@ CursosDeLosAlumnos.post("/", async (req, res) => {
     const { idAlumno, idsCursos } = req.body;
 
     const response = await postCursosDelAlumnoController(idAlumno, idsCursos);
+    res.status(200).json(response);
+  } catch (error: any) {
+    console.error(error.message);
+    res.status(500).json(error.message);
+  }
+});
+
+/!*------------------------------ ELIMINAR CURSOS DEL ALUMNO ---------------------------------*/;
+
+CursosDeLosAlumnos.delete("/:idAlumno/:idCurso", async (req, res) => {
+  try {
+    const { idAlumno, idCurso } = req.params;
+    const response = await deleteCursosDelAlumnoController(idAlumno, idCurso);
+    res.status(200).json(response);
+  } catch (error: any) {
+    console.error(error.message);
+    res.status(500).json(error.message);
+  }
+});
+
+/!*------------------------------ EDITAR LOS CURSOS DEL ALUMNO -------------------------------*/;
+
+CursosDeLosAlumnos.put("/", async (req, res) => {
+  try {
+    const { idAlumno, idsCursos } = req.body;
+
+    const response = await putCursosDelAlumnoController(idAlumno, idsCursos);
     res.status(200).json(response);
   } catch (error: any) {
     console.error(error.message);
